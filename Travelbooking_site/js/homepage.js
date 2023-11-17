38ddc0eab922b91777c783fa3d186d53f67bc3c6
SrinathG_Contus <srinath.g@contus.in>
2023-11-17 11:40:33 +0530

    first commit

---
+++ Travelbooking_site/js/homepage.js
@@ -0,0 +1,10 @@
+// Assuming you have an HTML element with an id of "myElement"
+const myElement = document.getElementById('menu-bars');
+const header = document.getElementById('header');
+
+// Adding a click event listener
+myElement.addEventListener('click', function() {
+  // Adding a class, for example, "newClass"
+  this.classList.toggle('active');
+  header.classList.toggle('active');
+});
